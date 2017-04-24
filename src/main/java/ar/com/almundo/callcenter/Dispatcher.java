c8d4d72682063712330869d1ad708c8bc685b7dd
Rafael Barrera Oro <borafael@gmail.com>
2017-04-24 02:29:49 -0300

    se implemento de manera basica la funcionalidad de la clase Dispatcher (atender llamadas)

--- src/main/java/ar/com/almundo/callcenter/Dispatcher.java
+++ src/main/java/ar/com/almundo/callcenter/Dispatcher.java
@@ -1,12 +1,35 @@
 package ar.com.almundo.callcenter;
 
+import ar.com.almundo.callcenter.call.Call;
+import ar.com.almundo.callcenter.call.CallHandler;
+import ar.com.almundo.callcenter.call.NoCallHandlerException;
+import ar.com.almundo.callcenter.employees.Director;
+import ar.com.almundo.callcenter.employees.Operator;
+import ar.com.almundo.callcenter.employees.Supervisor;
+
 public class Dispatcher {
 	
+	private CallHandler callHandler;
 	
-
-	public void dispatchCall(Call call) {
+	public Dispatcher() {
 		
+		CallHandler operatorCallHandler = new CallHandler(Operator.class);
+		CallHandler supervisorCallHandler = new CallHandler(Supervisor.class);
+		CallHandler directorCallHandler = new CallHandler(Director.class);
+		
+		operatorCallHandler.setNextHandler(supervisorCallHandler);
+		supervisorCallHandler.setNextHandler(directorCallHandler);
+		
+		this.callHandler = operatorCallHandler;
 	}
 	
-	
+	public void dispatchCall(Call call) {
+		
+		try {
+			callHandler.handleCall(call);
+		}
+		catch(NoCallHandlerException e) {
+			System.out.println("No se puede atender la llamada...");
+		}
+	}
 }
