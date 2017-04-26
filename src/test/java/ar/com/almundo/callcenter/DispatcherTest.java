15e045f470887f0664ebb29d87d18e4560f75a5e
Rafael Barrera Oro <borafael@gmail.com>
2017-04-26 03:47:32 -0300

    se borro el codigo duplicado de TetsCallThread, que existe en su propio archivo

--- src/test/java/ar/com/almundo/callcenter/DispatcherTest.java
+++ src/test/java/ar/com/almundo/callcenter/DispatcherTest.java
@@ -119,28 +119,4 @@ public class DispatcherTest extends TestCase {
 	private int getRandomCallDuration() {
 		return random.nextInt(MAX_CALL_DURATION - MIN_CALL_DURATION) + MIN_CALL_DURATION;
 	}
-	
-	private class TestCallThread extends Thread {
-		
-		private int callDuration;
-		
-		public TestCallThread(int callDuration) {
-			this.callDuration = callDuration;
-		}
-		
-		@Override
-		public void run() {
-
-			final Call call = new Call();
-			
-			Dispatcher.getInstance().dispatchCall(call);
-			
-			try {
-				Thread.sleep(callDuration);
-			} 
-			catch (InterruptedException ignore) {}
-			
-			call.end();
-		}
-	}
 }
